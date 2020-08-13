f3aab42a4c46a64d1041a6c7593e5b8d76875dad
qiujunxiong <webpackvue@163.com>
2020-08-13 18:12:52 +0800

    主页

---
+++ demo/src/components/common/navbar/NavTop.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navtop">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="rigth"><slot name="rigth"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavTop',
+  data(){
+      return{
+
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .navtop{
+      display: flex;
+      height: 44px;
+      line-height: 44px;
+      text-align: center;
+      box-shadow: 0px 1px 1px rgba(100,100,100,0.1);
+  }
+  .left,.rigth{
+      width: 60px;
+  }
+  .center{
+      flex: 1;
+  }
+</style>
--- demo/src/views/home/Home.vue
+++ demo/src/views/home/Home.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="home">
-    <h1>this is Home page</h1>
+    <nav-top class="home-nav">
+      <div slot="left">哈哈</div>
+      <div slot="center">购物车</div>
+      <div slot="right">aaaa</div>
+    </nav-top>
+
   </div>
 </template>
 
 <script>
+import NavTop from 'components/common/navbar/NavTop'
+import { getHomeMultidata } from 'network/data'
 
 export default {
   name: 'Home',
-  components: {
-    
+  components: { NavTop },
+  data(){
+    return{
+      banner: [],
+      recommends: []
+     
+    }
+  },
+  methods: {
+
+  },
+  created(){
+    getHomeMultidata().then(res => {
+      res = res.data;
+      // console.log(res)
+      if(res){
+        this.banner = res.banner.list;
+        this.recommends = res.recommends;
+      }
+      
+    })
   }
 }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
